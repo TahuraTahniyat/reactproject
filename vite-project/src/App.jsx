b07d207524e7734ebf70f68d82c2899d31d926b7
Tahura Tahniyat <tahniyattahura@gmail.com>
2024-10-18 11:48:20 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>React with Vite</h1>
+        <br />
+        <p> This is tahura from aiml-b section </p>
+        
+        </div>
+    </>
+  )
+}
+
+export default App
